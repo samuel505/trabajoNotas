807d8b9524db89cb5598b2af2460d912154c6bef
samuel505 <51140774+samuel505@users.noreply.github.com>
2022-10-24 19:50:35 +0200

    Commit inicial
    Algo de la parte de la vista hecha

---
+++ controllers/trabajoNotas.samuelBarros.controller.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+$data['titulo'] = "Calculos notas";
+$data["div_titulo"] = "Formulario";
+
+if (isset($_POST['enviar'])) {
+    
+}
+
+
+include 'views/templates/header.php';
+include 'views/trabajoNotas.samuelBarros.view.php';
+include 'views/templates/footer.php';
---
+++ views/trabajoNotas.samuelBarros.view.php
@@ -0,0 +1,54 @@
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800"><?php echo $data['titulo']; ?></h1>
+
+</div>
+
+<!-- Content Row -->
+
+<div class="row">
+
+    <div class="col-12">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary"><?php echo $data['div_titulo']; ?></h6>
+            </div>
+            <!-- Card Body -->
+            <div class="card-body">
+                <!--<form action="./?sec=formulario" method="post">                   -->
+                <form method="post" action="./?sec=trabajoNotas.samuelBarros">
+
+                    <div class="alert alert-success">
+
+
+                    </div>
+
+                    <div class="alert alert-warning">
+
+
+                    </div>
+
+                    <div class="alert alert-info">
+
+
+                    </div>
+
+                    <div class="alert alert-danger">
+
+
+                    </div>
+
+
+
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1">Inserta JSON</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" name="textarea" rows="3"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <input type="submit" value="Enviar" name="enviar" class="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
