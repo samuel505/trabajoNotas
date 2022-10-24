d1bd4029c54da5569ef527f69e976aa7806f4076
samuel505 <51140774+samuel505@users.noreply.github.com>
2022-10-24 22:48:50 +0200

    Arreglo fallos checkForm

--- controllers/trabajoNotas.samuelBarros.controller.php
+++ controllers/trabajoNotas.samuelBarros.controller.php
@@ -14,11 +14,12 @@ if (isset($_POST['enviar'])) {
 
 function checkForm($post): array
 {
+
     $errores = [];
-    if (count($post['json']) == 0) {
+    if (empty($post['json'])) {
         $errores['error'] = "El campo es obligatorio";
     } else {
-        $asignaturas = json_decode($post['json']);
+        $asignaturas = json_decode($post['json'], true);
         if (json_last_error() !== JSON_ERROR_NONE) {
             $errores['jsonError'] = "El formato del JSON es incorrecto";
         } else {
@@ -32,14 +33,16 @@ function checkForm($post): array
                     $jsonErrores[] = "La asignatura " . filter_var($asignatura, FILTER_SANITIZE_SPECIAL_CHARS) . " no tiene un array de alumnos";
                 } else {
                     foreach ($alumnos as $nombreAlumno => $nota) {
-                        if (empty($nombreAlumno)) {
-                            $jsonErrores[] = "En la asignatura " . filter_var($asignatura, FILTER_SANITIZE_SPECIAL_CHARS) . " hay un alumno sin nombre";
-                        }
-                        if (!is_int($nota)) {
-                            $jsonErrores[] = "La asignatura " . filter_var($asignatura, FILTER_SANITIZE_SPECIAL_CHARS) . "tiene una nota " . filter_var($nota, FILTER_SANITIZE_SPECIAL_CHARS) . " la cual no es un int";
-                        } else {
-                            if ($nota < 0 || $nota > 10) {
-                                $jsonErrores[] = "En la asignatura " . filter_var($asignatura, FILTER_SANITIZE_SPECIAL_CHARS) . ", alumno " . filter_var($nombreAlumno, FILTER_SANITIZE_SPECIAL_CHARS) . " tiene una nota de " . filter_var($nota, FILTER_SANITIZE_SPECIAL_CHARS);
+                        for ($i = 0; $i < count($nota); $i++) {
+                            if (empty($nombreAlumno)) {
+                                $jsonErrores[] = "En la asignatura " . filter_var($asignatura, FILTER_SANITIZE_SPECIAL_CHARS) . " hay un alumno sin nombre";
+                            }
+                            if (!is_numeric($nota[$i])) {
+                                $jsonErrores[] = "La asignatura " . filter_var($asignatura, FILTER_SANITIZE_SPECIAL_CHARS) . " tiene una nota " . filter_var($nota, FILTER_SANITIZE_SPECIAL_CHARS) . " la cual no es un numero";
+                            } else {
+                                if ($nota[$i] < 0 || $nota[$i] > 10) {
+                                    $jsonErrores[] = "En la asignatura " . filter_var($asignatura, FILTER_SANITIZE_SPECIAL_CHARS) . ", alumno " . filter_var($nombreAlumno, FILTER_SANITIZE_SPECIAL_CHARS) . " tiene una nota de " . filter_var($nota, FILTER_SANITIZE_SPECIAL_CHARS);
+                                }
                             }
                         }
                     }
